4bbf8d1f03a5bb1700431c90b19ffe0b3c90280f
v3ry <v3ryfr@gmail>
2022-08-22 12:06:49 +0200

    Adding challenge file

---
+++ challenge.ts
@@ -0,0 +1,20 @@
+// challenge.ts
+// let User: [string,number]
+interface User {
+    name : string,
+    age:number
+}
+const prettyPrintWilder = (users:User[]) :void=> {
+    users.map((user) => {
+      console.log(`${user.name} is ${user.age} years old`);
+    });
+  };
+  
+  const wilders:User[] = [];
+  const user1:User = { name: "Pierre", age: 23 };
+  const user2:User = { name: "Paul", age: 32 };
+  const user3:User = { name: "Jacques", age: 25 };
+  wilders.push(user1);
+  wilders.push(user2);
+  wilders.push(user3);
+  prettyPrintWilder(wilders);
